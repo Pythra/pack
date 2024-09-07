8cf886f9ee84b43cc377c362517b3496ff7031bc
unknown <peze336@gmail.com>
2024-09-07 15:19:03 +0500

    yes

--- packback/settings.py
+++ packback/settings.py
@@ -94,4 +94,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+ 
