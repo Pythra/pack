bdadc9ce63b8a32581cb7b1f70cdc25829caf6c4
unknown <peze336@gmail.com>
2024-09-13 01:32:00 +0500

    new

--- pack/admin.py
+++ pack/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Order, Product, Profile
+from .models import Order, Product, Profile, Cart, CartItem
 
 admin.site.register(Order)
 admin.site.register(Product)
 admin.site.register(Profile)
+admin.site.register(Cart)
+admin.site.register(CartItem)
