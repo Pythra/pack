b26913938245dc939a860ab871b00f5dc6e3bbb9
unknown <peze336@gmail.com>
2024-09-28 18:48:44 +0500

    :yes:

--- pack/admin.py
+++ pack/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, Product, Profile, AppItem, OrderItem
+from .models import Order, Product, Profile, AppItem, OrderItem, CartItem
 
 # Register the models to appear in the Django admin interface
 admin.site.register(Order)
@@ -7,3 +7,4 @@ admin.site.register(Product)
 admin.site.register(Profile)
 admin.site.register(AppItem)
 admin.site.register(OrderItem)
+admin.site.register(CartItem)
