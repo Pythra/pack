59e54f9cfaba366bb8ceb3b9639df007158ef80e
unknown <peze336@gmail.com>
2024-09-29 08:57:25 +0500

    yes

--- pack/apps.py
+++ pack/apps.py
@@ -6,5 +6,3 @@ class PackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pack'
 
-    def ready(self):
-        import pack.signals  # Ensure the signals are loaded
